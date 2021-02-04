0cd5e87a5a58a0306d8d43e873ffb85363737422
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-04 23:24:55 +0100

    Ejercicio 7 del UD5.

---
+++ src/Ejercicio7.java
@@ -0,0 +1,13 @@
+public class Ejercicio7 {
+	
+	public static void main(String args[]) {
+		
+		int num = 1;
+		
+		while (num <= 100){
+			System.out.println(num);
+			num++; //Incrementamos el valor de la variable num.
+		}//Definimos el bucle. 
+	}
+}
+
